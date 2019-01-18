cf80313ea36392ce7c105991dcda31dc4c16a331
linkepeng <linkepeng@aliyun.com>
2019-01-18 23:37:24 +0800

    1.Eureka 服务器注册发现

--- fortune-dubbo-learn/fortune-dubbo-provide/src/main/java/org/apache/dubbo/demo/provider/DemoServiceImpl.java
+++ fortune-dubbo-learn/fortune-dubbo-provide/src/main/java/org/apache/dubbo/demo/provider/DemoServiceImpl.java
@@ -4,11 +4,7 @@ import org.apache.dubbo.demo.DemoService;
 
 public class DemoServiceImpl implements DemoService {
 
-/*	@Override
-	public String sayHello(String name) {
-		return "Hello " + name;
-	}*/
-
+	@Override
 	public String sayHello(String name) {
 		return null;
 	}
